459ee6ee88a1bce0db679b7cf5e08025e2b47a47
GSaillenfest <g.saillenfest@gmail.com>
2024-07-17 00:15:20 +0000

    feat: url shortener and redirection from short url

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
 
-const cors = require('cors');
 const app = express();
-const dns = require('node:dns');
-const bodyParser = require('body-parser');
 const { error } = require('node:console');
 const { url } = require('node:inspector');
 const { stringify } = require('node:querystring');
-const urlencodedParser = bodyParser.urlencoded({ extended : false});
-
-const mongoose = require('mongoose');
 const { nextTick } = require('node:process');
+
+const dns = require('node:dns');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended : false});
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const client = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -19,31 +19,13 @@ const client = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 //mongoose model
 const URLSchema = new Schema({
   url: {type: String, required: true, unique: true},
 });
 const URLModel = mongoose.model('URLModel', URLSchema);
-
-const done = (err, data) => {
-  if (err) return console.log(err);
-  else {
-    console.log("Added: " + data);
-  }
-};
-const addUrl = () => {
-  const url_to_add = new URLModel({
-    url: requested_url,
-  })
-  url_to_add.save((err, data) => {
-    if (err) 
-      return done(err);
-    done(null, data);
-  });
-};
-
-
-app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -61,39 +43,48 @@ app.post('/api/shorturl', urlencodedParser, (req, res) => {
   //get url
   requested_url = req.body.url;
   //extract hostname from previous url
-  const requested_hostname = requested_url.replace(/^https?:\/\//, "")
-  //verify url validity and populate db
+  const requested_hostname = requested_url.replace(/^[a-z]*:\/\/?/, "").split('/')[0];
+  console.log("host: " + requested_hostname);
+
+  //verify url validity and add to db
   dns.lookup(requested_hostname, (err, address, family) => {
     if (err) {
-      res.json({
+      console.log("dns lookup error: " + err);
+      return res.json({
         error: 'invalid url'
       });
     }
     else {
       //add url to database
-      addUrl(requested_url)
-    }});
-  }, (req, res) => {
-      //get short_url
-      let id;
-      URLModel.findOne({url: requested_url}, (err, data) => {
-        if (err)
-          done(err);
-        id = data._id;
-      })
-      //respond json object
-      res.json({
-        original_url: requested_url,
-        short_url: id,
-      })
-    });
+      const url_to_add = new URLModel({
+        url: requested_url,
+      });
+      url_to_add.save((err, data) => {
+        if (err) {
+          return res.json({
+            error: 'invalid url'
+          });
+        }
+        //return json object
+        else {
+          console.log("7");
+          res.json({
+          original_url: requested_url,
+          short_url: data._id.toString(),
+          });
+        }
+      });
+    }
+  });
+});
 
 app.get('/api/shorturl/:url', (req, res) => {
-
+  console.log(req.params.url)
   URLModel.findById(req.params.url, (err, data) => {
     if (err)
-      done(err);
-    res.redirect(data.url);
+      return res.json({ error: "invalid url"});
+    else 
+      res.status(301).redirect(data.url);
   });
 });
 
